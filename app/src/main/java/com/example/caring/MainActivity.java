2a782e9d94aab60b08c9d5e60c0216e93f4bf54a
RajinduSE <rajinducooray98se@gmail.com>
2020-08-15 23:00:19 +0530

    Commit 3 rajindu

--- app/src/main/java/com/example/caring/MainActivity.java
+++ app/src/main/java/com/example/caring/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), InsertSubject.class);
+                Intent intent = new Intent(getApplicationContext(), ViewAchievement.class);
                 startActivity(intent);
             }
         });
