eb0f674c96e232c6290710fec6ff24b6fdd4b864
Your Name <dilumividanage1@gmail.com>
2020-08-15 23:10:58 +0530

    timetable

---
+++ app/src/main/java/com/example/caring/Update_Timetable.java
@@ -0,0 +1,14 @@
+package com.example.caring;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Update_Timetable extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update__timetable);
+    }
+}
