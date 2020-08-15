c696ff170383edb4b26b1ef53c2b55466a71b5ae
Dilmi101 <dnimasha971@gmail.com>
2020-08-15 23:19:16 +0530

    dilmi added

---
+++ app/src/main/java/com/example/caring/ViewVaccine.java
@@ -0,0 +1,14 @@
+package com.example.caring;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewVaccine extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_vaccine);
+    }
+}
