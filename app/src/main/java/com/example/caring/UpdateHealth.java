aae7512b38e861d5d31d624af99c02a9a8081a66
Dilmi101 <dnimasha971@gmail.com>
2020-10-04 10:08:49 +0530

    Back button added

--- app/src/main/java/com/example/caring/UpdateHealth.java
+++ app/src/main/java/com/example/caring/UpdateHealth.java
@@ -3,7 +3,11 @@ package com.example.caring;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -40,5 +44,33 @@ public class UpdateHealth extends AppCompatActivity {
         allergies.setText(health.getAllergies());
 
 
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.option_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.dashboard:
+                startActivity(new Intent(this, MainDashboard.class));
+                return true;
+            case R.id.education:
+                startActivity(new Intent(this, EducationDashboard.class));
+                return true;
+            case R.id.health:
+                startActivity(new Intent(this, Dashboard.class));
+                return true;
+            case R.id.achievement:
+                startActivity(new Intent(this, ViewAchievement.class));
+                return true;
+            case R.id.timeTable:
+                startActivity(new Intent(this, ViewTimetable.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
